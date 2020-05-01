6023501e3cf625b080ed38786b6b470381bd1b66
ashick1910 <bavashashick@gmail.com>
2020-05-01 14:29:45 +0530

    java git bash

---
+++ Stringx.java
@@ -0,0 +1,17 @@
+package stringfunction;
+
+public class Stringx {
+	
+	public static void main(String[] args) {
+		
+		String s= "JAVA";
+		String s1="JAVA";
+		String s2="java";
+		boolean e = s.equals(s1);
+		System.out.println(e);
+		boolean c = s.equalsIgnoreCase(s2);
+		System.out.println(c);
+		
+	}
+
+}
